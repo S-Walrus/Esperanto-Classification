605d7005179869430086c024d4a6323f3dd2d4e5
s-walrus <swalrus@yandex.ru>
2019-10-08 11:24:44 +0300

    Modified alias generation script

--- generate_alias_map.py
+++ generate_alias_map.py
@@ -3,20 +3,26 @@ import pickle
 import re
 from tqdm import tqdm
 
-with open('data/aliases') as f:
-	d = {re.search(r'<http://rdf\.freebase\.com/ns/m\.([a-z0-9_]+)>.*', s).group(1):
-			re.search(r'"(.*)"@en', s).group(1) for s in f}
+# rdf_id_pattern = \
+#         re.compile(r'<http://rdf\.freebase\.com/ns/m\.([a-z0-9_]+)>.*')
+rdf_alias_pattern = re.compile(r'"(.*)"@en')
+rdf_id_pattern = re.compile(r'([0-9a-z_]+)')
+fb_id_pattern = re.compile(r'www\.freebase\.com/m/([a-z0-9_]+).*')
+
+with open('data/labels') as f:
+    d = {re.search(rdf_id_pattern, s).group(1):
+         re.search(rdf_alias_pattern, s).group(1) for s in tqdm(f)}
 with open('data/subjects.txt') as f:
-	res = []
-	for s in tqdm(f):
-		if re.match(r'www\.freebase\.com/m/([a-z0-9_]+).*'):
-			res.append(d[re.match(r'www\.freebase\.com/m/([a-z0-9_]+).*').group(1)])
-		else:
-			res.append('__@@__')
+    res = []
+    for s in tqdm(f):
+        if re.match(fb_id_pattern, s):
+            res.append(d[re.match(fb_id_pattern, s).group(1)])
+        else:
+            res.append('__@@__')
 
 out = np.array(res)
 print('Success!')
 print(out)
 
 with open('data/compiled_aliases', 'wb') as f:
-	pickle.dump(out, f)
+    pickle.dump(out, f)
